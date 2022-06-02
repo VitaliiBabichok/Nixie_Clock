c196cd8d899b43a9a3ebcfd9f1385ea7127a7160
Vitalik_Babichok <vitalii.babichok.ki.2018@lpnu.ua>
2022-06-02 22:47:51 +0300

    Added func poointer for modes clock

--- nixie/nixie.ino
+++ nixie/nixie.ino
@@ -3,6 +3,12 @@
 #include <WiFiUdp.h>
 #include "init.h"
 
+int Hour;
+int Minute;
+int Second;
+
+int Minute_Start;
+int Second_Start;
 
 TaskHandle_t HandleSettingClock;
 TaskHandle_t HandleClock;
@@ -17,11 +23,53 @@ typedef enum command_clock_t {
 };
 command_clock_t Set = COMMAND_CLOCK;
 
+typedef void (*general_function) (const int arg1, const int arg2);
+
+void set_clock(const int HH, const int MM) //format HH:MM
+{
+  parseIndicator(HH, MM);
+}
+
+void set_stopwatch(const int MM, const int SS) //format MM:SS
+{
+  //func stop time;
+
+  //func reset time;
+
+  //func exit mode;
+  parseIndicator(MM-Minute_Start, SS-Second_Start);
+}
+
+void set_timer(const int MM, const int SS) //format MM:SS
+{
+  //func stop time;
+
+  //func reset time;
+
+  //func check time set and real time
+    //func buzzer
+    
+  //func exit mode;
+  parseIndicator(Minute_Start-MM, Second_Start-SS);
+}
+
+void set_alarm(const int HH, const int MM) //format HH:MM
+{
+  //func check time set and real time
+    //func buzzer
+
+  // func exit mode    
+}
+
+void set_mode (general_function f, const int Time1, const int Time2)
+{
+  f(Time1,Time2) ;    // call the passed-in function
+}
+
 TimerHandle_t xTimer = NULL;
 
-int Hour;
-int Minute;
-int Second=50;
+
+
 
 
 void setup() {
@@ -50,12 +98,6 @@ void setup() {
 
 void increment_time(TimerHandle_t xTimer) {
 
-  Serial.print("Time: ");
-    Serial.print(Hour);
-    Serial.print(":");
-    Serial.print(Minute);
-    Serial.print(":");
-    Serial.println(Second);
   Second++;
   if (Second > MAX_SECOND)
   {
@@ -81,6 +123,41 @@ void get_time() {
   Minute = timeClient.getMinutes();
   Second = timeClient.getSeconds();
   Hour = timeClient.getHours();
+}
+void  nixieWriteIndicator(uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t value) {
+  if (d == NULL) // Digit range 0 to 5
+  {
+    //    digitalWrite(c, (value & 0x04) >> 2);
+    //    digitalWrite(b, (value & 0x02) >> 1);
+    //    digitalWrite(a, value & 0x01);
+    Serial.println("C B A");
+    Serial.println(String((value & 0x04) >> 2) + " " + String((value & 0x02) >> 1) + " " + String(value & 0x01));
+  }
+  else // Digit range 0 to 9
+  {
+    //    digitalWrite(d, (value & 0x08) >> 3);
+    //    digitalWrite(c, (value & 0x04) >> 2);
+    //    digitalWrite(b, (value & 0x02) >> 1);
+    //    digitalWrite(a, value & 0x01);
+    Serial.println("D C B A");
+    Serial.println(String((value & 0x08) >> 3) + " " + String((value & 0x04) >> 2) + " " + String((value & 0x02) >> 1) + " " + String(value & 0x01));
+  }
+
+}
+
+void parseIndicator(int Time1, int Time2) {
+
+  Serial.println(Time1);
+  int Time1_H = Time1 / 10;
+  nixieWriteIndicator(A_H1, B_H1, C_H1, NULL , Time1_H);
+  int Time1_L = Time1 % 10;
+  nixieWriteIndicator(A_L2, B_L2, C_L2, D_L2, Time1_L);
+
+  Serial.println(Time2);
+  int Time2_H = Time2 / 10;
+  nixieWriteIndicator(A_H3, B_H3, C_H3, NULL, Time2_H);
+  int Time2_L = Time2 % 10;
+  nixieWriteIndicator(A_L4, B_L4, C_L4, D_L4, Time2_L);
 }
 void loop() {
 
